5298f8c1f15598c3b316e5025d1fe0fd0fcf398d
DESKTOP-BEAR877\sfsfk <sfsfkj@gmail.com>
2019-03-10 23:29:22 +0900

    crolling and html parsing use bs4

--- getHtml.py
+++ getHtml.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import requests
 
 def get_html(url):
@@ -8,8 +9,16 @@ def get_html(url):
     return output    
 
 
-url = "http://www.statiz.co.kr/player.php?opt=10&name=%EC%B5%9C%EC%A0%95&birth=1987-02-28"
+url = "http://www.statiz.co.kr/player.php?opt=6&sopt=0&name=%EC%B5%9C%EC%A0%95&birth=1987-02-28&re=0&da=1&year=2018&plist=&pdate="
 text = get_html(url)
+soup = BeautifulSoup(text, 'html.parser')
+data = soup.find("table",{"class":"table-striped"}).find_all("td") #가까운 table을 찾고 그 안에 모든 td를 추출, 반환형이 string 아님
 f = open('output/sample.txt',mode='wt',encoding='utf-8')
-f.write(text)
+cnt = 0
+for n in data: #n 자체에 해당 테그정보까지 담고있음
+    cnt += 1    
+    f.write("%s    "%n.get_text()) #내용만 추출
+    if cnt%14 == 0:
+        f.write("\n")
+        cnt = 0
 f.close()

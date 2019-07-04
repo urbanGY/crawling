675af7bc375b7147e2b29688024423876a0b5740
Hyun Seok Kim <sfsfkj@gmail.com>
2019-07-04 14:02:35 +0900

    add other movie

--- crawling.py
+++ crawling.py
@@ -64,41 +64,43 @@ chrome_option.add_argument('lang=ko_KR') #selenium 에서 gui 작동 안하게 h
 driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_option) #chrome driver 사용 및 headless 옵션 객체 적용
 driver.implicitly_wait(3) #랜더링 완료 시간 대기
 
-site_name = 'watcha'
-doc_id = 0
-content_url = ''
-category = 'movie'
-title = '아이언맨 3' #항목 생성하려는 대상 이름
-actor = ''
-summary = ''
-score = ''
-review = ''
+list = ['아이언맨 3', '아이언맨 2', '아이언맨', '닥터 스트레인지', '7광구']
+for n in list:
+    site_name = 'watcha'
+    doc_id = 0
+    content_url = ''
+    category = 'movie'
+    title = n #항목 생성하려는 대상 이름
+    actor = ''
+    summary = ''
+    score = ''
+    review = ''
 
-url = 'https://watcha.com/ko-KR' #crawling 하려는 사이트 url
-xpath_watcha = '//*[@id="root"]/div/div[1]/section/section/section[2]/div[2]/div[1]/div/div/div/ul/li[@*]/a[@title="'+title+'"]' #watcha 사이트의 세부 정보 링크 xpath, xpath 문법 중요
+    url = 'https://watcha.com/ko-KR' #crawling 하려는 사이트 url
+    xpath_watcha = '//*[@id="root"]/div/div[1]/section/section/section[2]/div[2]/div[1]/div/div/div/ul/li[@*]/a[@title="'+title+'"]' #watcha 사이트의 세부 정보 링크 xpath, xpath 문법 중요
 
-page, content_url = get_html(url, title, xpath_watcha) #html 과 url링크 가져옴
-score = get_score(page) #별점
-actor = get_actorlist(page) # 배우
-summary = get_summary(page) # 내용
+    page, content_url = get_html(url, title, xpath_watcha) #html 과 url링크 가져옴
+    score = get_score(page) #별점
+    actor = get_actorlist(page) # 배우
+    summary = get_summary(page) # 내용
 
-print('****************************************')
-print('사이트명 : ', site_name)
-print('doc Id : ', doc_id)
-print('url : ',content_url)
-print('category : ', category)
-print('title : ', title)
-print('actor : ', actor)
-print('summary : ', summary)
-print('score : ', score)
+    print('****************************************')
+    print('사이트명 : ', site_name)
+    print('doc Id : ', doc_id)
+    print('url : ',content_url)
+    print('category : ', category)
+    print('title : ', title)
+    print('actor : ', actor)
+    print('summary : ', summary)
+    print('score : ', score)
 
-json_file = OrderedDict()
-json_file["site_name"] = site_name
-json_file["doc_id"] = doc_id
-json_file["url"] = content_url
-json_file["category"] = category
-json_file["data"] = {'title' : title, 'actor' : actor, 'summary' : summary}
-json_file["score"] = score
+    json_file = OrderedDict()
+    json_file["site_name"] = site_name
+    json_file["doc_id"] = doc_id
+    json_file["url"] = content_url
+    json_file["category"] = category
+    json_file["data"] = {'title' : title, 'actor' : actor, 'summary' : summary}
+    json_file["score"] = score
 
-with open('./data/movie/'+site_name+'_'+title+'.json', 'w', encoding='utf-8') as make_file:
-    json.dump(json_file, make_file, ensure_ascii=False, indent="\t")
+    with open('./data/movie/'+site_name+'_'+title+'.json', 'w', encoding='utf-8') as make_file:
+        json.dump(json_file, make_file, ensure_ascii=False, indent="\t")

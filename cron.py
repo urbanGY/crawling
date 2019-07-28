e65da16b083e865c6e4ec2110323503cac9475bd
Hyun Seok Kim <sfsfkj@gmail.com>
2019-07-28 21:50:36 +0900

    add cron

---
+++ cron.py
@@ -0,0 +1,10 @@
+from . import link_crawling
+
+def movie_schedule():
+    # movie_list = link_crawling.read_movie_list_test()
+    # site_list = link_crawling.read_site_list()
+    # date = link_crawling.body(site_list, movie_list)
+    f = open('movie_log.txt',mode='wt',newline='')
+    f.write('gg')
+    f.close()
+    pass
